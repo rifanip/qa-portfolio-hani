d87c13e9ffb59bac2d02d74329b8003593674da3
Hanifah <aku.anip@gmail.com>
2025-06-26 12:40:11 +0700

    Update CustomerCreation.js

--- Cypress/Support/step_definitions/CustomerCreation.js
+++ Cypress/Support/step_definitions/CustomerCreation.js
@@ -1,5 +1,5 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-import CustomerElements from "../object_repository/CreateNewCustomerElements";
+import CustomerElements from "../object_repository/CustomerElements";
 import 'cypress-real-events/support'
 import 'cypress-file-upload'
 
@@ -21,105 +21,105 @@ afterEach(() => {
 });
 
 Given ('User is on LOS main page', ()=>{
-    CreateNewCustomerElements.LoanMainPage().should('be.visible')
+    CustomerElements.LoanMainPage().should('be.visible')
     cy.wait(1000)
 })
 
 When ('user clicks on New Customer', ()=>{
     cy.url().should('not.include', '/login')
-    CreateNewCustomerElements.AddNewbutton().click()
+    CustomerElements.AddNewbutton().click()
     cy.wait('@checkToken').its('response.statusCode').should('eq', 200)
-    CreateNewCustomerElements.Loader().should('not.be.visible')
+    CustomerElements.Loader().should('not.be.visible')
 })
 
 Then ('user will direct to the New Customer Page', ()=>{
-    CreateNewCustomerElements.NewCustomerPage()
+    CustomerElements.NewCustomerPage()
 })
 
 When ('user clicks Save button', ()=>{
-    CreateNewCustomerElements.savebutton().should('be.visible').click()
+    CustomerElements.savebutton().should('be.visible').click()
 })
 
 When ('user submit the customer', ()=>{
-    CreateNewCustomerElements.savebutton().should('be.visible').click()
-    CreateNewCustomerElements.Loader().should('not.be.visible')
+    CustomerElements.savebutton().should('be.visible').click()
+    CustomerElements.Loader().should('not.be.visible')
     cy.wait('@postCustomerCreation').its('response.statusCode').should('eq', 200)
 })
 
 Then ('Users will see the required fields cannot be empty', ()=>{
-    CreateNewCustomerElements.mandatorychecking().should('be.visible')
+    CustomerElements.mandatorychecking().should('be.visible')
 })
 
 Then ('user input customer name {string} and {string} and {string}', (firstname, middlename, lastname)=>{
     const fullName = '${firstname} ${middlename} ${lastname}'
     cy.wrap(fullName).as('customerName')
-    CreateNewCustomerElements.firstname().type(firstname)
-    CreateNewCustomerElements.Middlename().type(middlename)
-    CreateNewCustomerElements.lastname().type(lastname)
+    CustomerElements.firstname().type(firstname)
+    CustomerElements.Middlename().type(middlename)
+    CustomerElements.lastname().type(lastname)
 })
 
 Then ('user will see toast message {string}', ()=>{
-    CreateNewCustomerElements.successtoastmessage().should('be.visible')
+    CustomerElements.successtoastmessage().should('be.visible')
 })
 
 When ('user clicks Cancel button', ()=>{
-    CreateNewCustomerElements.cancelbutton().click()
-    CreateNewCustomerElements.Loader().should('not.be.visible')
+    CustomerElements.cancelbutton().click()
+    CustomerElements.Loader().should('not.be.visible')
 })
 
 Then ('Users will direct to the main page', ()=>{
-    CreateNewCustomerElements.LoanMainPage()
+    CustomerElements.LoanMainPage()
 })
 
 When ('user clicks on View button', ()=>{
-    CreateNewCustomerElements.viewCustomer().click()
-    CreateNewCustomerElements.Loader().should('not.be.visible')
+    CustomerElements.viewCustomer().click()
+    CustomerElements.Loader().should('not.be.visible')
 })
 
 Then ('user will see the customer added successfully', function(){
     cy.get('@customerName').then((fullName) => {
-    CreateNewCustomerElements.customerVerify().invoke('text').should('contain', fullName)
+    CustomerElements.customerVerify().invoke('text').should('contain', fullName)
     })
 })
 
 When ('user fill the type of app', ()=>{    
-    CreateNewCustomerElements.typeofapp().click()
+    CustomerElements.typeofapp().click()
     cy.xpath("//button[.='Active Accounts']").click()
-    CreateNewCustomerElements.customersource().click()
+    CustomerElements.customersource().click()
     cy.contains('button.dropdown-item', 'SM (Others)').click()
 })
 
 When ('user add identification ID', ()=>{
-    CreateNewCustomerElements.ID().click({force: true})
+    CustomerElements.ID().click({force: true})
     cy.contains('button.dropdown-item', 'Philhealth').click()
     cy.wait(1000)
     cy.get("[placeholder='Enter Philhealth Number']").scrollIntoView().should('be.visible').type('1234')
     // uploadimage
     cy.get('input[type="file"].inputFile').selectFile('cypress/fixtures/test.png', { force: true })
-    CreateNewCustomerElements.Loader().should('not.be.visible')
+    CustomerElements.Loader().should('not.be.visible')
     cy.wait('@postIdentify').its('response.statusCode').should('eq', 200)
     cy.wait('@getIdentify').its('response.statusCode').should('eq', 200)
 })
 
 When ('user fill all the mandatory fields', ()=>{
-    CreateNewCustomerElements.branch().should('not.contain', 'Select Branch')
-    CreateNewCustomerElements.salesagent().should('not.contain', 'Select Sales Agent')
-    CreateNewCustomerElements.portofolio().click()
+    CustomerElements.branch().should('not.contain', 'Select Branch')
+    CustomerElements.salesagent().should('not.contain', 'Select Sales Agent')
+    CustomerElements.portofolio().click()
     cy.contains('button.dropdown-item', 'Two Wheeler').click()
-    CreateNewCustomerElements.purposeofpurchase().click()
+    CustomerElements.purposeofpurchase().click()
     cy.contains('button.dropdown-item', 'Business').click()
-    CreateNewCustomerElements.interest().click()
+    CustomerElements.interest().click()
     cy.contains('button.dropdown-item', 'Cold').click()
-    CreateNewCustomerElements.suffix().click()
+    CustomerElements.suffix().click()
     cy.xpath("//button[.='Jr.']").click({ force: true })
-    CreateNewCustomerElements.dob().click()
+    CustomerElements.dob().click()
     cy.get('.theme-datepicker-header-year-selector .arrow-icon').eq(0).click();
     cy.xpath("//span[.='2000']").click()
     cy.get('.theme-datepicker-header-month-selector .arrow-icon').eq(0).click()
     cy.xpath("//span[.='Feb']").click()
     cy.get('.react-datepicker__day--023').click()
     // validate age
-    CreateNewCustomerElements.dob().invoke('val').then((birthdateStr) => {
+    CustomerElements.dob().invoke('val').then((birthdateStr) => {
     // format DD/MM/YYYY)
     const [day, month, year] = birthdateStr.split('/');
     const dob = new Date(`${year}-${month}-${day}`);
@@ -133,40 +133,40 @@ When ('user fill all the mandatory fields', ()=>{
     // Age Validation
     cy.get("[placeholder='Enter Age']").should('have.value', expectedAge.toString());
     })
-    CreateNewCustomerElements.mobilenoOwner().click()
+    CustomerElements.mobilenoOwner().click()
     cy.contains('button.dropdown-item', 'Applicant').click()
-    CreateNewCustomerElements.checklist().click()
+    CustomerElements.checklist().click()
 })
 
 Then ('user upload selfie image', ()=>{
-    CreateNewCustomerElements.Image().first().selectFile('cypress/fixtures/example.png', { force: true });
-    CreateNewCustomerElements.Loader().should('not.be.visible')
+    CustomerElements.Image().first().selectFile('cypress/fixtures/example.png', { force: true });
+    CustomerElements.Loader().should('not.be.visible')
     cy.wait('@postSelfie').its('response.statusCode').should('eq', 200)
     cy.wait('@getSelfie').its('response.statusCode').should('eq', 200)
     cy.wait(2000)
 })
 When ('User input mobile number less than 10 digits', ()=>{
-    CreateNewCustomerElements.mobileno().type('91182222')
-    CreateNewCustomerElements.savebutton().click()
+    CustomerElements.mobileno().type('91182222')
+    CustomerElements.savebutton().click()
     cy.xpath("//span[.='Invalid Number']").should('be.visible')
-    CreateNewCustomerElements.mobileno().clear().type('9167836271')
+    CustomerElements.mobileno().clear().type('9167836271')
     cy.xpath("//span[.='Invalid Number']").should('not.exist')
 })
 When ('User input wrong email format', ()=>{
-    CreateNewCustomerElements.email().type('email@add')
+    CustomerElements.email().type('email@add')
     cy.xpath("//span[.='This Email is Invalid']").should('be.visible')
-    CreateNewCustomerElements.email().type('.com')
+    CustomerElements.email().type('.com')
     cy.xpath("//span[.='This Email is Invalid']").should('not.exist')
 })
 When ('User Add social media account', ()=>{
-    CreateNewCustomerElements.socmeddropdown().click()
+    CustomerElements.socmeddropdown().click()
     cy.xpath("//button[.='Twitter']").click()
-    CreateNewCustomerElements.socmedlink().type('test social media')
-    CreateNewCustomerElements.addsocmed().click()
-    CreateNewCustomerElements.deletesocmed().should('be.visible')
+    CustomerElements.socmedlink().type('test social media')
+    CustomerElements.addsocmed().click()
+    CustomerElements.deletesocmed().should('be.visible')
 })
 
 Then ('user will see toast message Profile has been updated successfully', ()=>{
     cy.contains('div', 'Profile has been updated successfully')
-    CreateNewCustomerElements.AddNewbutton().should('be.visible')
+    CustomerElements.AddNewbutton().should('be.visible')
 })
